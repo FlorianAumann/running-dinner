96083267362b0553b1c22859c3a78aa147f6b289
Florian Aumann <florianaumann@gmail.com>
2025-03-31 20:54:51 +0200

    Added xlsx input test
    Added a test file as an example for xlsx tests and added two unit tests
    for this functionality.

--- testing/tests.py
+++ testing/tests.py
@@ -5,6 +5,7 @@ import unittest
 from src.config import ConfigManager
 from src.data.geoLocation import GeoLocation
 from src.googleapi.googleApi import GoogleApi
+from src.xlsx.xlsxInput import read_teams_from_xlsx
 
 
 class TestGoogleAPI(unittest.TestCase):
@@ -31,3 +32,20 @@ class TestGoogleAPI(unittest.TestCase):
             self.google_api.download_path_map(locations, "red", 2, mapfile)
             # We can't really test a lot here, but the file should at least exist after the method execution
             self.assertTrue(os.path.isfile(mapfile))
+
+
+class TestXLSXInput(unittest.TestCase):
+    def test_input_file_missing(self):
+        dinner_teams = read_teams_from_xlsx(os.getcwd() + "\\nonexisting.xlsx")
+        self.assertIsNone(dinner_teams)
+
+    def test_read_xlsx(self):
+        dinner_teams = read_teams_from_xlsx(os.getcwd() + "\\Test.xlsx")
+        self.assertEqual(3, len(dinner_teams))
+        self.assertEqual("Rue Joliot Curie, 03390 Montmarault, France", dinner_teams[1].address)
+        self.assertEqual("17 Rue de Montaigut, 03390 Montmarault, France", dinner_teams[2].address)
+        self.assertEqual(2, len(dinner_teams[2].participants))
+        self.assertEqual("George@test.com", dinner_teams[2].participants[1].email)
+        self.assertEqual("George", dinner_teams[2].participants[1].firstName)
+        self.assertEqual("4", dinner_teams[0].participants[1].phone)
+        self.assertEqual("None", dinner_teams[1].participants[0].food_restrictions)
