e4bf05b1a735175f86c64b584f42ba3d0823d3ee
Florian Aumann <florianaumann@gmail.com>
2025-04-15 11:04:38 +0200

    Small refactoring & documentation
    Added more documentation to the Solution class and refactored the
    get_paths_per_host method for readadbility.

--- src/planning/solution.py
+++ src/planning/solution.py
@@ -42,6 +42,10 @@ class Solution(object):
         of guests that join the cooking team for this course. The indices in this list refer to the list for the other
         courses, where the actual team number can be found
 
+        The representation of a solution is kept in a way where the solution can be changed via simple permutation of
+        indices and will always still stay a valid solution, allowing for easy mutation of a solution to arrive at new
+        versions of it for our genetic algorithm.
+
         :param groups_per_course: For each course, defines the dinner groups which will cook and eat together
                                   Dimensions: courses x group
         """
@@ -56,6 +60,12 @@ class Solution(object):
                     raise TypeError("Guest index list must be number of courses minus one!")
 
     def get_paths_per_host(self) -> {int: [int]}:
+        """
+        From the internal representation of the solution, create a readable representation of dinner paths per host
+        A dinner path consists of a list of host numbers that describe where a team will eat for each course
+
+        :return Returns a map that contains the dinner paths for each host :
+        """
         paths_per_host = {}
         # For the cooking hosts in the first course group, we just use the indices the following courses to get their path
         for group_idx in range(len(self.groups_per_course[0])):
@@ -63,19 +73,29 @@ class Solution(object):
             for course_idx in range(1, len(self.groups_per_course)):
                 paths_per_host[self.groups_per_course[0][group_idx].cooking_team].append(self.groups_per_course[course_idx][group_idx].cooking_team)
 
-        for course_idx in range(1, len(self.groups_per_course)):
-            for group_idx in range(len(self.groups_per_course[course_idx])):
-                host = self.groups_per_course[course_idx][group_idx].cooking_team
+        # Iterate over all remaining courses and all groups in each course
+        for active_course_idx in range(1, len(self.groups_per_course)):
+            for group_idx in range(len(self.groups_per_course[active_course_idx])):
+                # For each of the cooking hosts in there, create a new empty list
+                host = self.groups_per_course[active_course_idx][group_idx].cooking_team
                 paths_per_host[host] = []
-                for course_idx2 in range(len(self.groups_per_course)):
-                    if course_idx2 == course_idx:
+                # Now iterate over all courses again to fill the path list for the current host
+                for course_idx in range(len(self.groups_per_course)):
+                    if course_idx == active_course_idx:
+                        # In case the current course is also the active course index, the current team will be the one cooking
+                        # -> Simply add its own index to the list
                         paths_per_host[host].append(host)
                     else:
-                        if course_idx < course_idx2:
-                            host_index = self.groups_per_course[course_idx2][group_idx].guest_indices[course_idx]
+                        # Look up the host_index under where to find the actual guest number in the other courses
+                        # Since the current course is not part of the groups_per_course list, we need to decrement
+                        # all indices above the current one by one
+                        if active_course_idx < course_idx:
+                            guest_course_idx = active_course_idx
                         else:
-                            host_index = self.groups_per_course[course_idx2][group_idx].guest_indices[course_idx - 1]
-                        paths_per_host[host].append(self.groups_per_course[course_idx2][host_index].cooking_team)
+                            guest_course_idx = active_course_idx - 1
+                        host_index = self.groups_per_course[course_idx][group_idx].guest_indices[guest_course_idx]
+                        # Using this host index, look up which cooking team the current host will join for this course
+                        paths_per_host[host].append(self.groups_per_course[course_idx][host_index].cooking_team)
         return paths_per_host
 
     def __eq__(self, other):
