c3d978d1719c2a396d4425df24b78ebda8ba5924
Florian Aumann <florianaumann@gmail.com>
2025-04-15 15:06:23 +0200

    Documentation for DiversitySolutionRater
    Added a few more comments to the DiversitySolutionRater and refactored
    for readability

--- src/planning/rating.py
+++ src/planning/rating.py
@@ -32,18 +32,22 @@ class DiversitySolutionRater(SolutionRater):
         :param paths_per_host: The solution to rate
         :return: A score between 0 and 1
         """
+        # Iterate over all teams and count the overlaps with other teams (one overlap is always accepted)
         overlaps = 0
-        course_count = len(paths_per_host[0])
-        teams_per_course_count = len(paths_per_host)
-        maximum_overlaps = (course_count - 1) * (course_count - 1) * teams_per_course_count
         for team1 in range(len(paths_per_host)):
             for team2 in range(len(paths_per_host)):
                 if team1 != team2:
                     # Get intersection between both team's paths
-                    intersections = collections.Counter(paths_per_host[team1]) & collections.Counter(
-                        paths_per_host[team2])
+                    intersections = collections.Counter(paths_per_host[team1]) & collections.Counter(paths_per_host[team2])
                     # If more than one intersection exists, add additional intersection to count
                     if len(intersections) > 1:
                         overlaps += (len(intersections) - 1)
+        # Calculate the maximum overlaps possible for this solution size
+        course_count = len(paths_per_host[0])
+        teams_per_course_count = len(paths_per_host)
+        maximum_overlaps = (course_count - 1) * (course_count - 1) * teams_per_course_count
+        # Normalize this score to be between 0 and 1
+        # Then subtract it from 1.0, so it gets better the less overlaps we have
+        # In the end, square the result so bad solutions decrease the result exponentially, not linearly
         score = (1.0 - (overlaps / maximum_overlaps)) ** 2
         return score
